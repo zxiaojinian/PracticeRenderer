2f92192a9969c5c505a52f113cf5523b49c3db2e
zxj <111>
2023-07-31 00:33:22 +0800

    初始工程

---
+++ premake5.lua
@@ -0,0 +1,82 @@
+workspace "PracticeRenderer"
+    architecture "x86_64"
+    startproject "PracticeRenderer"
+
+    configurations 
+    { 
+        "Debug", 
+        "Release", 
+    }
+
+    flags
+    {
+        "MultiProcessorCompile"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+group "Dependencies"
+    include "PracticeRenderer/Dependencies/GLFW"
+    include "PracticeRenderer/Dependencies/Glad"
+    include "PracticeRenderer/Dependencies/imgui"
+group ""
+
+project "PracticeRenderer"
+    location "PracticeRenderer"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "pch.h"
+    pchsource "%{prj.name}/Source/pch.cpp"
+
+    files 
+    { 
+        "%{prj.name}/Source/**.h", 
+        "%{prj.name}/Source/**.cpp",
+        "%{prj.name}/Dependencies/stb_image/**.h",
+        "%{prj.name}/Dependencies/stb_image/**.cpp",
+        "%{prj.name}/Dependencies/glm/glm/**.hpp",
+        "%{prj.name}/Dependencies/glm/glm/**.inl",
+    }
+
+    defines
+    {
+        --"_CRT_SECURE_NO_WARNINGS"
+        "GLFW_INCLUDE_NONE"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src",
+        "%{prj.name}/Dependencies/GLFW/include",
+        "%{prj.name}/Dependencies/Glad/include",
+        "%{prj.name}/Dependencies/imgui",
+        "%{prj.name}/Dependencies/glm",
+        "%{prj.name}/Dependencies/stb_image"
+    }
+
+    links 
+    { 
+        "GLFW",
+        "Glad",
+        "ImGui",
+        "opengl32.lib"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "PR_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "PR_RELEASE"
+        runtime "Release"
+        optimize "on"

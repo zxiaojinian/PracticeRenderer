56e6a9e010040dae6b9e12c0347bc259fa4b126f
zxj <111>
2023-08-10 18:34:57 +0800

    Transform

--- PracticeRenderer/Source/Core/Scene/Components/Transform.cpp
+++ PracticeRenderer/Source/Core/Scene/Components/Transform.cpp
@@ -2,6 +2,8 @@
 #include "Transform.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 
 namespace PR
 {
@@ -15,74 +17,126 @@ namespace PR
 	{
 	}
 
-	void Transform::SetLocalEulerAngles()
+	glm::vec3 Transform::GetLocalEulerAngles() const
 	{
+		return glm::eulerAngles(m_LocalRotation);
 	}
 
-	void Transform::SetLocalPosition()
+	glm::vec3 Transform::GetEulerAngles() const
 	{
+		return glm::eulerAngles(m_Rotation);
 	}
 
-	void Transform::SetLocalScale()
+	void Transform::SetLocalEulerAngles(const glm::vec3& angle)
 	{
+		m_LocalRotation = glm::qua<float>(glm::radians(angle));
+		OnLocalChange();
 	}
 
-	void Transform::SetLocalRotation()
+	void Transform::SetLocalPosition(const glm::vec3& pos)
 	{
+		m_LocalPosition = pos;
+		OnLocalChange();
 	}
 
-	void Transform::SetPosition()
+	void Transform::SetLocalScale(const glm::vec3& scale)
 	{
+		m_LocalScale = scale;
+		OnLocalChange();
 	}
 
-	void Transform::SetScale()
+	void Transform::SetLocalRotation(const glm::qua<float>& rotation)
 	{
+		m_LocalRotation = rotation;
+		OnLocalChange();
 	}
 
-	void Transform::SetRotation()
+	void Transform::SetEulerAngles(const glm::vec3& angle)
 	{
+		m_Rotation = glm::qua<float>(glm::radians(angle));
+		OnWorldChange();
 	}
 
-	const glm::vec3& Transform::GetLocalForward()
+	void Transform::SetPosition(const glm::vec3& pos)
 	{
-		// TODO: 在此处插入 return 语句
+		m_Position = pos;
+		OnWorldChange();
 	}
 
-	const glm::vec3& Transform::GetLocalRight()
+	void Transform::SetScale(const glm::vec3& scale)
 	{
-		// TODO: 在此处插入 return 语句
+		m_Scale = scale;
+		OnWorldChange();
 	}
 
-	const glm::vec3& Transform::GetLocalUp()
+	void Transform::SetRotation(const glm::qua<float>& rotation)
 	{
-		// TODO: 在此处插入 return 语句
+		m_Rotation = rotation;
+		OnWorldChange();
 	}
 
-	const glm::vec3& Transform::GetForward()
+
+	glm::mat4 Transform::GetWorldToLocalMatrix() const
 	{
-		// TODO: 在此处插入 return 语句
+		return glm::inverse(m_LocalToWorldMatrix);
 	}
 
-	const glm::vec3& Transform::GetRight()
+	glm::vec3 Transform::GetLocalForward()
 	{
-		// TODO: 在此处插入 return 语句
+		return glm::vec3();
 	}
 
-	const glm::vec3& Transform::GetUp()
+	glm::vec3 Transform::GetLocalRight()
 	{
-		// TODO: 在此处插入 return 语句
+		return glm::vec3();
+	}
+
+	glm::vec3 Transform::GetLocalUp()
+	{
+		return glm::vec3();
+	}
+
+	glm::vec3 Transform::GetForward()
+	{
+		return glm::vec3();
+	}
+
+	glm::vec3 Transform::GetRight()
+	{
+		return glm::vec3();
+	}
+
+	glm::vec3 Transform::GetUp()
+	{
+		return glm::vec3();
+	}
+
+
+	void Transform::OnLocalChange()
+	{
+		m_LocalToParentMatrix = glm::translate(glm::mat4(1.0f), m_LocalPosition) * glm::mat4_cast(m_LocalRotation) * glm::scale(glm::mat4(1.0f), m_LocalScale);
+		UpdateWorldMatrix();
+	}
+
+	void Transform::OnWorldChange()
+	{
+		m_LocalToWorldMatrix = glm::translate(glm::mat4(1.0f), m_Position) * glm::mat4_cast(m_Rotation) * glm::scale(glm::mat4(1.0f), m_Scale);
+		UpdateLoclaMatrix();
 	}
 
 	void Transform::UpdateLoclaMatrix()
 	{
-		//TODO 替换为四元数
-		const glm::mat4 rotateX = glm::rotate(glm::mat4(1.0f), glm::radians(m_LocalRotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
-		const glm::mat4 rotateY = glm::rotate(glm::mat4(1.0f), glm::radians(m_LocalRotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-		const glm::mat4 rotateZ = glm::rotate(glm::mat4(1.0f), glm::radians(m_LocalRotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
-		// Y * X * Z
-		const glm::mat4 roationMatrix = rotateY * rotateX * rotateZ;
-
-		m_LocalToParentMatrix = glm::translate(glm::mat4(1.0f), m_LocalPosition) * roationMatrix * glm::scale(glm::mat4(1.0f), m_LocalScale);
+		if (m_Parent)
+		{
+			m_LocalToParentMatrix = glm::inverse(m_Parent->m_LocalToWorldMatrix) * m_LocalToWorldMatrix;
+		}
+		else
+		{
+			m_LocalToParentMatrix = m_LocalToWorldMatrix;
+			m_LocalPosition = m_Position;
+			m_LocalRotation = m_Rotation;
+			m_LocalScale = m_Scale;
+		}
 	}
 
 	void Transform::UpdateWorldMatrix()
@@ -90,11 +144,30 @@ namespace PR
 		if (m_Parent)
 		{
 			m_LocalToWorldMatrix = m_Parent->m_LocalToWorldMatrix * m_LocalToParentMatrix;
+			DecomposeMatrix(m_LocalToWorldMatrix, m_Position, m_Rotation, m_Scale);
 		}
 		else
 		{
 			m_LocalToWorldMatrix = m_LocalToParentMatrix;
+			m_Position = m_LocalPosition;
+			m_Rotation = m_LocalRotation;
+			m_Scale = m_LocalScale;
 		}
+		//TODO更新子物体transform
+	}
+
+	void Transform::DecomposeMatrix(const glm::mat4& m, glm::vec3& pos, glm::quat& rotation, glm::vec3& scale)
+	{
+		pos = m[3];
+
+		for (int i = 0; i < 3; i++)
+			scale[i] = glm::length(glm::vec3(m[i]));
+
+		const glm::mat3 rotationMatrix(
+			scale[0] ? glm::vec3(m[0]) : glm::vec3(m[0]) / scale[0],
+			scale[1] ? glm::vec3(m[1]) : glm::vec3(m[1]) / scale[1],
+			scale[2] ? glm::vec3(m[2]) : glm::vec3(m[2]) / scale[2]);
+		rotation = glm::quat_cast(rotationMatrix);
 	}
 
 }
--- PracticeRenderer/Source/Core/Scene/Components/Transform.h
+++ PracticeRenderer/Source/Core/Scene/Components/Transform.h
@@ -14,55 +14,59 @@ namespace PR
 
 		void SetParent(Transform& parent);
 
-		const glm::vec3& GetLocalEulerAngles() const { return m_LocalRotation; }
+		glm::vec3 GetLocalEulerAngles() const;
 		const glm::vec3& GetLocalPosition() const { return m_LocalPosition; }
 		const glm::vec3& GetLocalScale() const { return m_LocalScale; }
-		/*const glm::qua<float>& GetLocalRotation();*/
-		const glm::vec3& GetLocalRotation() const { return m_LocalRotation; }
+		const glm::qua<float>& GetLocalRotation() const { return m_LocalRotation; }
 
+		glm::vec3 GetEulerAngles() const;
 		const glm::vec3& GetPosition() const { return m_Position; }
 		const glm::vec3& GetScale() const { return m_Scale; }
-		/*const glm::qua<float>& GetRotation();*/
-		const glm::vec3& GetRotation() const { return m_Rotation; }
+		const glm::qua<float>& GetRotation() const { return m_Rotation; }
 
-		void SetLocalEulerAngles();
-		void SetLocalPosition();
-		void SetLocalScale();
-		void SetLocalRotation();
+		void SetLocalEulerAngles(const glm::vec3& angle);
+		void SetLocalPosition(const glm::vec3& pos);
+		void SetLocalScale(const glm::vec3& scale);
+		void SetLocalRotation(const glm::qua<float>& rotation);
 
-		void SetPosition();
-		void SetScale();
-		void SetRotation();
+		void SetEulerAngles(const glm::vec3& angle);
+		void SetPosition(const glm::vec3& pos);
+		void SetScale(const glm::vec3& scale);
+		void SetRotation(const glm::qua<float>& rotation);
 
 		const glm::mat4& GetLocalToWorldMatrix() const { return m_LocalToWorldMatrix; }
-		const glm::mat4& GetWorldToLocalMatrix() const { return m_WorldToLocalMatrix; }
+		glm::mat4 GetWorldToLocalMatrix() const;
 
-		const glm::vec3& GetLocalForward();
-		const glm::vec3& GetLocalRight();
-		const glm::vec3& GetLocalUp();
+		glm::vec3 GetLocalForward();
+		glm::vec3 GetLocalRight();
+		glm::vec3 GetLocalUp();
 
-		const glm::vec3& GetForward();
-		const glm::vec3& GetRight();
-		const glm::vec3& GetUp();
+		glm::vec3 GetForward();
+		glm::vec3 GetRight();
+		glm::vec3 GetUp();
 
 	private:
+		void OnLocalChange();
+		void OnWorldChange();
+
 		void UpdateLoclaMatrix();
 		void UpdateWorldMatrix();
+
+		void DecomposeMatrix(const glm::mat4& m, glm::vec3& pos, glm::quat& rot, glm::vec3& scale);
+
 
 	private:
 		Transform* m_Parent = nullptr;
 
 		glm::vec3 m_LocalPosition = { 0.0f, 0.0f, 0.0f };
 		glm::vec3 m_LocalScale = { 1.0f, 1.0f, 1.0f };
-		glm::vec3 m_LocalRotation = { 0.0f, 0.0f, 0.0f };
+		glm::qua<float> m_LocalRotation;
 
 		glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
 		glm::vec3 m_Scale = { 1.0f, 1.0f, 1.0f };
-		glm::vec3 m_Rotation = { 0.0f, 0.0f, 0.0f };
+		glm::qua<float> m_Rotation;
 
 		glm::mat4 m_LocalToWorldMatrix = glm::mat4(1.0f);
-		glm::mat4 m_WorldToLocalMatrix = glm::mat4(1.0f);
-
 		glm::mat4 m_LocalToParentMatrix = glm::mat4(1.0f);
 	};
 }

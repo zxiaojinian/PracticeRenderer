63e1208c486b86491620520c888d605bd962689f
zxj <111>
2023-11-29 17:37:07 +0800

    CSM-记录一些问题

--- PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/ShadowCasterPass.h
+++ PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/ShadowCasterPass.h
@@ -7,13 +7,22 @@
 #include <glm/glm.hpp>
 
 //Projective Aliasing --csm
-//Shadow Acne--light direction depth bias + normal direction bias
+
+//Shadow Acne
+//	1.light direction depth bias + normal direction bias（bias需要根据阴影纹理纹素覆盖范围来缩放）
+//	2.缩小阴影投射视锥体近裁剪面和远裁剪面距离，提升阴影纹理精度
+ 
 //Shadow swimming.--根本原因是场景像素帧与帧之间采样到阴影纹理深度值发生变化。
 	//1.旋转相机,阴影视锥体大小发生变化，导致阴影纹理纹素覆盖的世界空间范围发生变化，需要固定阴影视锥体大小。
 	//2.移动相机，如果2帧之间，阴影纹理纹素中心移动距离不是其覆盖的世界空间范围的整数倍就会发生闪烁，需要以阴影纹理的纹素覆盖的世界空间范围来吸附阴影视锥体中心位置
+ 
 //PCF--存在2个问题
 	//1，PCF样本可能在该级联范围外，如果是纹理数组，设置寻找方式为clamp应该没问题。如果是图集，需要为该级联外扩一半pcf范围
-	//2，PCF样本范围过大，会加剧Shadow Acne，因为和阴影纹理样本比较不是其对应几何物体的光空间深度。
+	//2，PCF样本范围过大，会加剧Shadow Acne，因为和阴影纹理样本比较不是其对应几何物体的光空间深度。这时需要更大的bias
+
+//级联间的混合，一种性能较好的方式，抖动（https://catlikecoding.com/unity/tutorials/custom-srp/directional-shadows/#4.8）
+
+//级联间有重合区域，是不是可以把重合区域物体从较大级联区域渲染中剔除来提升性能？
 namespace PR
 {
 	#define MAX_CASCADES 4

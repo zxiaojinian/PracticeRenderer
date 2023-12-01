43ce46b2bbe84f701b77ed18879e37dd29c0fdc7
zxj <111>
2023-12-01 18:38:30 +0800

    后处理

--- PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/PostProcessPass.cpp
+++ PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/PostProcessPass.cpp
@@ -10,6 +10,29 @@ namespace PR
 
 	void PostProcessPass::Execute(GraphicsContext& graphicsContext, const RenderingData& renderingData)
 	{
+		uint32_t w = renderingData.cameraData.pixelRect.z / 2;
+		uint32_t h = renderingData.cameraData.pixelRect.w / 2;
+
+		uint32_t mipMapCount = static_cast<uint32_t>( std::floor(std::log2( (std::max)(w, h) )) );
+		const uint32_t minMipCount = 0;
+		mipMapCount = std::clamp(mipMapCount, minMipCount, k_MaxMipCount);
+		if (w != m_InitWidth || h != m_InitHeight)
+		{
+			m_InitWidth = w;
+			m_InitHeight = h;
+
+			m_BloomMipTexture.clear();
+			RenderTextureSpecification colorSpecification = { w, h, 1, TextureFormat::R11G11B10_UFloatPack32, TextureWrapMode::Clamp, TextureFilterMode::Bilinear, false };
+			for (uint32_t mipIndex = 0; mipIndex < mipMapCount; ++mipIndex)
+			{
+				colorSpecification.Width = w;
+				colorSpecification.Height = h;
+				w /= 2;
+				h /= 2;
+				auto mipTexture = RenderTexture::Create("mipTex" + mipIndex, colorSpecification);
+				m_BloomMipTexture.push_back(mipTexture);
+			}
+		}
 		//draw fullscreen meshs
 		graphicsContext.SetBackBuffer();
 		graphicsContext.SetViewport(renderingData.cameraData.pixelRect);
--- PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/PostProcessPass.h
+++ PracticeRenderer/Source/Core/Render/RenderPipeline/RenderPass/PostProcessPass.h
@@ -15,6 +15,12 @@ namespace PR
 	private:
 		std::shared_ptr<Material> m_BlitMaterial;
 		std::shared_ptr<RenderTexture> m_Source;
+
+		//bloom
+		const uint32_t k_MaxMipCount = 16;
+		uint32_t m_InitWidth = 0;
+		uint32_t m_InitHeight = 0;
+		std::vector<std::shared_ptr<RenderTexture>> m_BloomMipTexture;
 	};
 }
 
